b0c609bd5fe995c2b8fde3e38175fdf415e724c9
Mohamed Galal <44075680+dpkg9510@users.noreply.github.com>
2020-08-25 12:47:59 +0300

    Add files via upload

---
+++ pwd.py
@@ -0,0 +1,12 @@
+import subprocess
+
+# The text that will be copied to the clipboard if the secret code is valid
+pwd = "PUT_YOUR_TEXT_HERE"
+pwd_input = int(input("Secret_Code:"))
+
+# Replace 12 with your preferred code
+if pwd_input == 12:
+    subprocess.run("clip", universal_newlines=True, input=pwd)
+    print("success: copied to clipboard")
+else:
+    print("Secret_Code is Invalid")
